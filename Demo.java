0d7ce724fdc1b67ee6a5ce99a660fa8ffdfb13b7
unknown <hitender.shekhawat@flipkart.com>
2022-01-18 11:49:15 +0530

    first commit

---
+++ Demo.java
@@ -0,0 +1,10 @@
+package mypakage;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("first java proj in eclipse");
+	}
+
+}
